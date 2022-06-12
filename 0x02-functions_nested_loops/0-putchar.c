198983ebe2bfae18b91f82c556c02777ba62ca8a
ayodejibidemi <bidemiayodeji@yahoo.com>
2022-06-12 08:31:19 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,11 @@
-#include "holberton.h"
-
+#include <unistd.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always returns 0
- *     *
- */
+ * *main - Entry point
+ * *
+ * *Return: always 0
+ * */
 int main(void)
 {
-		_putchar('H');
-			_putchar('o');
-				_putchar('l');
-					_putchar('b');
-						_putchar('e');
-							_putchar('r');
-								_putchar('t');
-									_putchar('o');
-										_putchar('n');
-											_putchar('\n');
-												return (0);
+		write(1,"_putchar\n", 57);
+			return (0);
 }
